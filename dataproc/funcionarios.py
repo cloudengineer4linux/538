25f71ffed434777a16ab0461046b6781eee67393
4Linux <67426067+curso-4linux@users.noreply.github.com>
2023-04-26 16:00:18 -0300

    Update funcionarios.py

--- dataproc/funcionarios.py
+++ dataproc/funcionarios.py
@@ -3,12 +3,12 @@ import pandas
 
 spark = SparkSession.builder.appName("Test").getOrCreate()
 
-funcionarios = spark.read.csv("gs://roberto_farias_database/export_usuarios.csv")
+funcionarios = spark.read.csv("gs://database_cloud_engineer_seu_nome/exports/export_usuarios.csv")
 column_list = ['id','nome','departamento','salario','email','senha','cadastro']
 funcionarios = funcionarios.toDF(*column_list)
 
 funcionarios.createOrReplaceTempView("funcionarios")
 
-sqlSALARIOS = spark.sql("SELECT * FROM funcionarios WHERE Salario > 5000").write.mode('Overwrite').csv('gs://funcionarios-pyspark/output_csv')
+sqlSALARIOS = spark.sql("SELECT * FROM funcionarios WHERE Salario > 5000").write.mode('Overwrite').csv('gs://dataproc_funcionarios_pyspark_seu_nome/output_csv')
 
-sqlSALARIOS = spark.sql("SELECT * FROM funcionarios WHERE Salario > 5000").write.mode('Overwrite').json('gs://funcionarios-pyspark/output_json')
+sqlSALARIOS = spark.sql("SELECT * FROM funcionarios WHERE Salario > 5000").write.mode('Overwrite').json('gs://dataproc_funcionarios_pyspark_seu_nome/output_json')
